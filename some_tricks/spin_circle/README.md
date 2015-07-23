1b4a80b28cfc869d68444bc0329f67883cd2adba
Bulat Kultash <dream@punktinitiative.ru>
2015-07-23 19:43:30 +0300

    New repo

---
+++ some_tricks/spin_circle/README.md
@@ -0,0 +1,8 @@
+# spin circle
+## Spin Rotation
+ We need something to rotate.
+
+ Let`s do it!
+
+I will update this repo sometimes...
+
---
+++ some_tricks/spin_circle/spin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <title>ROTATION</title>
+    <meta name="Keywords" content="" />
+    <meta name="Description" content="" />
+    <meta name="language" content="en_US" />
+    <link rel="stylesheet" type="text/css"  href="spin.css">
+
+</head>
+<body>
+
+<div class="circle">
+    <img src="circle-black.png" alt="circle" class="spin"/>
+</div>
+</body>
+</html>
