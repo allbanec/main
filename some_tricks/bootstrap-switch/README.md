be1094f2669aa3f6d4c7db96733298d9b8961655
Bulat Kultash <dream@punktinitiative.ru>
2015-07-28 14:19:11 +0300

    Full ready Styled Bootstrap Switch

---
+++ some_tricks/bootstrap-switch/README.md
@@ -0,0 +1,38 @@
+# Bootstrap Switch
+## Styled Bootstrap Switch
+
+ The task is: to make a styled switch for one interface....
+
+ I used [Bootstrap Switch 3.3.2](https://github.com/nostalgiaz/bootstrap-switch)
+
+ Thank You for owner!
+
+ Bootstrap Switch work`s good! Realy good...
+
+ But it was not enough to close my task...
+
+ I need to change the style...
+
+ OK!
+
+ I use SASS for my style. But you can see CSS file only if you need.
+
+ I blocked standard Bootstrap Switch CSS and change it in my SCSS file and put inside two new blocks with ON and OFF labels...
+
+ If the switch is on, one of the labels toggle`s them class "active"(use simple Jquery) and styles are change`s...
+
+ DONE!
+
+ You can see the working version in index.php file.
+
+ Look`s same like this:
+
+ ![Preview image](preview.png)
+
+
+
+
+
+
+
+
---
+++ some_tricks/bootstrap-switch/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Styled Bootstrap Switch</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.3.2/css/bootstrap3/bootstrap-switch.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.3.2/js/bootstrap-switch.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="switch">
+    <input type="checkbox" name="switch" checked>
+    <div class="on-switch button-on active">ON</div>
+    <div class="on-switch button-off">OFF</div>
+</div>
+<script>
+    $("[name='switch']").bootstrapSwitch().on('switchChange.bootstrapSwitch', function() {
+        $(".on-switch").toggleClass("active")
+    });
+</script>
+</body>
+</html>
