7e2e783255da944ee4c1861ca396194201498bf4
Bulat Kultash <dream@punktinitiative.ru>
2015-08-11 14:04:17 +0300

    1. 1_version 2. 2_version

--- some_tricks/spin_circle/1_version/spin.php
+++ some_tricks/spin_circle/1_version/spin.php
@@ -5,15 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <title>ROTATION</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <title>ROTATION 1</title>
     <meta name="Keywords" content="" />
     <meta name="Description" content="" />
     <meta name="language" content="en_US" />
     <link rel="stylesheet" type="text/css"  href="spin.css">
-
 </head>
 <body>
-
 <div class="circle">
     <img src="circle-black.png" alt="circle" class="spin"/>
 </div>
---
+++ some_tricks/spin_circle/2_version/spin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <title>ROTATION 2</title>
+    <meta name="Keywords" content="" />
+    <meta name="Description" content="" />
+    <meta name="language" content="en_US" />
+    <link rel="stylesheet" type="text/css"  href="spin.css">
+</head>
+<body>
+<div class="circle">
+    <img src="timer.png" alt="circle" class="spin"/>
+    <img src="point.png" alt="dot" class="dot"/>
+</div>
+</body>
+</html>
